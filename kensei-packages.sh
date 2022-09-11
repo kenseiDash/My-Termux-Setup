abc78c5ebdfbdc40a526c3447b311ea82fb1c4e0
kenseiDash <75147295+kenseiDash@users.noreply.github.com>
2022-09-11 01:18:24 -0300

    Add files via upload

---
+++ kensei-packages.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+#Get the necessary components
+ 
+apt install nmap -y
+apt install fish -y
+apt install cowsay -y
+apt install vim -y
+apt install wcalc -y
+apt install bmon -y
+apt install proot -y
+apt install golang -y
+apt install neofetch -y
+apt install vim-gtk -y
+apt install curl -y
+apt install git -y
+apt install wget -y
+apt install zip -y
+apt install unzip -y
+apt install python -y
+apt install python2 -y
+apt install nodejs -y
+apt install jq -y
+apt install libxml2-utils -y
+apt install grep -y
+apt install bc -y
+apt install htop -y
+apt install figlet -y
+apt install httping -y
+apt install dnsutils -y
+apt install openssh -y
+apt install ffmpeg -y
+apt install nano -y
+apt install zsh -y
+apt install php -y
+apt install clang -y
+apt install libffi -y
+apt install openssl -y
+apt install w3m -y
+apt install perl -y
+apt install ruby -y
+apt install rust -y
+apt instal tar -y
+curl -sL https://gist.githubusercontent.com/mskian/6ea9c2b32d5f41867e7cafc88d1b26d5/raw/youtube-dl.sh | bash
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/data/data/com.termux/files/usr/bin --filename=composer
+pip install --upgrade pip setuptools
+pip install --upgrade httpie
+pip install -U requests[socks]
+pip install requests
+pip install ddgr
